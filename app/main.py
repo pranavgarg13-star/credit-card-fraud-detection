00a22d3a2719d955cd9ed561d8ec52cd935222d0
Pranav <pranavgarg1603@gmail.com>
2026-03-31 14:20:33 +0530

    fix static/index.html

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi import Query, HTTPException
-from pydantic import BaseModel
 import joblib
 import numpy as np
 import os
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import pandas as pd
+from app.schemas import TransactionInput
 app = FastAPI()
 
 # After app = FastAPI(...), add:
@@ -46,41 +46,6 @@ FEATURE_ORDER = [
 HIGH_RISK_THRESHOLD = 0.8
 MEDIUM_RISK_THRESHOLD = 0.5
 
-# Input schema
-from pydantic import BaseModel
-
-class Transaction(BaseModel):
-    Time: float
-    V1: float
-    V2: float
-    V3: float
-    V4: float
-    V5: float
-    V6: float
-    V7: float
-    V8: float
-    V9: float
-    V10: float
-    V11: float
-    V12: float
-    V13: float
-    V14: float
-    V15: float
-    V16: float
-    V17: float
-    V18: float
-    V19: float
-    V20: float
-    V21: float
-    V22: float
-    V23: float
-    V24: float
-    V25: float
-    V26: float
-    V27: float
-    V28: float
-    Amount: float
-
 
 
 @app.get("/health")
@@ -92,7 +57,7 @@ def health():
 
 @app.post("/predict")
 def predict_fraud(
-    data: Transaction,
+    data: TransactionInput,
     model: str = Query("rf", enum=["rf", "logistic"])
 ):
     selected_model = MODEL_REGISTRY.get(model)
